7de11e6ab40f5abb578dab8d6343b47893f0958d
Yann Klein <yann.klein@me.com>
2024-05-16 17:22:35 +0900

    add a 200 response to line_receiver

--- pages/api/line_receiver.ts
+++ pages/api/line_receiver.ts
@@ -2,4 +2,5 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   console.log(req.body);
+  return res.status(200).send("Ok");
 }
