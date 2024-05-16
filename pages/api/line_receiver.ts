8b7dc4b70e6796c45ba9da014e97d219893d7664
Yann Klein <yann.klein@me.com>
2024-05-16 17:48:12 +0900

    add message details into lin receiver logs

--- pages/api/line_receiver.ts
+++ pages/api/line_receiver.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   console.log({body: req.body});
-  console.log({message: req.body.message});
+  console.log({message: req.body.events[0].message});
+  console.log({message: req.body.events[0].source});
   return res.status(200).send("Ok");
 }
